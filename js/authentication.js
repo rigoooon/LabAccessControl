b16684cd5dd495242158c792b7ee5c66bf58508b
rigoooon <92728062+rigoooon@users.noreply.github.com>
2022-02-24 22:36:36 +0900

    Update authentication.js

--- js/authentication.js
+++ js/authentication.js
@@ -29,7 +29,6 @@ var uiConfig = {
   signInSuccessUrl: '../public/signup.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
   ],
   // Terms of service url.利用規約
